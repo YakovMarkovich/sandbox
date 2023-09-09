640a0b81bc90a9c8a88cc27be37beadcc793e66b
Yakov Markovich <yakob.markovich@gmail.com>
2023-09-09 09:52:25 +0300

    divide-one-decimal-by-another-with-javascript

---
+++ freeCodeCamp/basicJavascript/divide-one-decimal-by-another-with-javascript.js
@@ -0,0 +1,11 @@
+//❓ DESCRIPTION:
+/*
+Now let's divide one decimal by another.
+
+Change the 0.0 so that quotient will equal to 2.2.
+*/
+
+
+//🟩 SOLUTION:
+const quotient = 4.4 / 2.0; // Change this line
+console.log("quotient ", quotient)
