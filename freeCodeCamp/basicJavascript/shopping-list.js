ebed7646bc6587a60f054fe584150ef094c46f0a
Yakov Markovich <yakob.markovich@gmail.com>
2023-09-09 12:27:37 +0300

    shopping-list

---
+++ freeCodeCamp/basicJavascript/shopping-list.js
@@ -0,0 +1,20 @@
+//❓ DESCRIPTION:
+/*
+Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list.
+*/
+
+
+
+//🟩 SOLUTION:
+const myList = [];
+myList.push(["Chocolate Bar", 15]);
+myList.push(["Bread", 10]);
+myList.push(["Butter", 12]);
+myList.push(["Cheese", 35]);
+myList.push(["Rasberry", 11]);
+console.log("myList ", myList);
